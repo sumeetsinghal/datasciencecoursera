e3e2a3d0ba4212159a6367fa77e6cd46804c26a5
sumeetsinghal <singhalsumeet@gmail.com>
2020-03-10 02:22:28 +0530

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -1,85 +1,67 @@
+install.packages("dplyr")
 library(dplyr)
-
-#rename the file as per feature
-getname <- function(x) {
-
-  name <- unlist(strsplit(x, "_"))
-  y<- length(name)
-  newname <- ""
-  i=1
-  while(i <y) {
-    if(i+1 ==y) newname <- paste0(newname, name[i])
-    else newname <- paste0(newname, name[i],"_")
-    i=i+1
-   }
-   newname 
   
-  }
+# load the features from the features file  
+  
+  path <- "/Users/sumeetsinghal/sumeet/Coursera/datacleaningweek4/UCIHARDataset"
+  setwd(path)
+  features <- read.table("features.txt", header = F, stringsAsFactors=F)
+
+# load the activity from the features file  
+
+  activity <- read.table("activity_labels.txt", header = F, stringsAsFactors=F)
+  activity[3,2]
+# load the test data  
+  path <- "/Users/sumeetsinghal/sumeet/Coursera/datacleaningweek4/UCIHARDataset/test"
+  setwd(path)
+  data_file1 <- read.table("X_test.txt", header = F, stringsAsFactors=F)
+
+# load and aggregate the test data from three data frame
+  names(data_file1) <- features[[2]]
+  data_file2 <- read.table("y_test.txt", header = F, stringsAsFactors=F ,col.names = "Activity")
+  data_file3 <- read.table("subject_test.txt", header = F, stringsAsFactors=F ,col.names = "Subject")
+  data_file4 <- cbind(data_file3,data_file2,data_file1) 
+
+  # load the train data  
+
+  getwd()
+  path <- "/Users/sumeetsinghal/sumeet/Coursera/datacleaningweek4/UCIHARDataset/train"
+  setwd(path)
 
 
-# Process each file in directory, merge each column and aggregate the data thru each file
-processeachDir <- function(path, fileName, aggdataf) {
-  data_file <- read.table(file=paste0(path,"/", filename),header = F, stringsAsFactors=F)
-  length <- nrow(data_file)
-  newdataf <- data.frame()
-  i=1
-  while(i <= length) {
-    meanv <- mean(c(unlist(data_file[i,])), na.rm= TRUE)
-    sdv <- sd(c(unlist(data_file[i,])), na.rm= TRUE)
-    if(!is.na(sdv)) 
-      newdataf[i,paste0("SD_",getname(fileName))] <- sdv
-      
-    i <i + 1
-  }
+  data_file5 <- read.table("X_train.txt", header = F, stringsAsFactors=F)
+
+# add the features name to the dataset
+  names(data_file5) <- features[[2]]
+
+# load and aggregate the training data from three data frame
+
+  data_file6 <- read.table("y_train.txt", header = F, stringsAsFactors=F ,,col.names = "Activity")
+  data_file7 <- read.table("subject_train.txt", header = F, stringsAsFactors=F,col.names = "Subject")
+  data_file8 <- cbind(data_file7,data_file6,data_file5) 
+  
+  #aggregate the test and train data
+  data_file9 <-  rbind(data_file4,data_file8)
+  
+# filter only the mean and Standard devition data
+  featuresfilter <- grep("*mean()|std()[-]*" , features[[2]])
+  features[[2]][featuresfilter]
+  data_file10 <- data_file9[, c("Subject","Activity", features[[2]][featuresfilter])]
+  newlabel <-  lapply(data_file10[, "Activity"], replace <- function(x) activity[x,2])
+                      
+ # add activity names replacing with activity code                     
+  data_file10[, "Activity"] <- newlabel
+  data_file10
+  
+ # group by activity and subject                     
+  aggdata <- data_file10 %>% group_by(Subject, Activity)
+  aggdata
+ 
+ #write to file                     
+  path <- "/Users/sumeetsinghal/sumeet/Coursera/datacleaningweek4/UCIHARDataset"
+  setwd(path)
+  write.table(aggdata, file = "data")
   
   
-  newdataf
-  if(nrow(aggdataf) ==0) {
-    aggdataf <- newdataf
-  else   
-      aggdataf <- cbind(aggdataf, newdataf)
-   aggdataf
-    
-}
-
-# traverse through all the directory and process each file
-  findallpath <- function(path, aggdataf1) {
-    fs <- list.file(path)
-    for(fileName in fs) {
-      if(dir.exists(fileName))
-        aggdataf1 <- findallpath(paste0(path,"/",fileName), aggdataf1)
-      else   
-         aggdataf1 <- processeachDir(path,fileName,aggdataf1)
-         aggdataf1
-    }
-       aggdataf1
-   }
-
   
-  # this is execution of the main code
   
-  #aggregate the training data
-  setwd("c:/workarea/train")
-  path <- "c:/workarea/train"
-  aggdataf2 <- data.frame()
-  aggdataf1 <- data.frame()
-  aggdataf1 <- findallpath(path,aggdataf2)
-  
-  #aggregate the test data
-  setwd("c:/workarea/test")
-  path <- "c:/workarea/test"
-  aggdataf3 <- data.frame()
-  aggdataf4 <- data.frame()
-  aggdataf3 <- findallpath(path,aggdataf4)
-    
-  #Merging both data
-  aggdataf5 <- data.frame()
-  aggdataf5 <- rbind(aggdataf1,aggdataf3)
-  aggdataf5
-  
-  # aggregate group by the subject
-  aggdata <- aggdataf5 %>% group_by(mean_subject)
-
-
-
-
